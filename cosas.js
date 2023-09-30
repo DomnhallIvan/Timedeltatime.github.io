ff8ac1c4b0ba8c268b7594188dc87c78b204d9d0
Ivan PM <cdmx2125@amerike.edu.mx>
2023-09-29 20:31:18 -0600

    Ez Reloj

--- cosas.js
+++ cosas.js
@@ -1,75 +1,23 @@
 
-class Poligono_Regular_Shido {
-    constructor(lados) {
-        this.lados = lados;
+function Reloj_instantaneo() {
+    let fecha = new Date();
+    let hora = fecha.getHours();
+    let minutos = fecha.getMinutes();
+    let segundos = fecha.getSeconds();
+    if(hora >= 12)
+    {
+        console.log(`Son las ${hora} horas pm`);
+        console.log(`Con ${minutos} minutos `);
+        console.log(`Y ${segundos} segundos `);
     }
-
-    obtenerPerimetro() {
-        return this.lados.reduce((a, b) => a + b, 0);
+    else
+    {
+        console.log(`Son las ${hora} horas am`);
+        console.log(`Con ${minutos} minutos `);
+        console.log(`Y ${segundos} segundos `);
     }
+    
+  }
 
-    obtenerArea() {
-        return "Fallo en Área";
-    }
-}
-
-class Cuadrado extends Poligono_Regular_Shido {
-    obtenerArea() {
-        if (this.lados.length === 4) {
-            return this.lados[0] ** 2;
-        } else {
-            return "Fallo en Área";
-        }
-    }
-
-    obtenerPerimetro() {
-        return this.lados[0] * 4;
-    }
-}
-
-class Triangulo extends Poligono_Regular_Shido {
-    obtenerArea() {
-        if (this.lados.length === 3) {
-            const [a, b, c] = this.lados;
-            const l = this.obtenerPerimetro() / 2;
-            return Math.sqrt(l * (l - a) * (l - b) * (l - c));
-        } else {
-            return "Fallo en Área";
-        }
-    }
-
-    obtenerPerimetro() {
-        return this.lados[0]*3;
-    }
-}
-
-
-
-class Pentagono extends Poligono_Regular_Shido {
-    obtenerArea() {
-        if (this.lados.length === 5) {
-            const lado = this.lados[0];
-            return (5 * lado ** 2) / (4 * Math.tan(Math.PI / 5));
-        } else {
-            return "Bruh";
-        }
-    }
-
-    obtenerPerimetro() {
-        return this.lados[0] * 5;
-    }
-}
-
-
-const triangulo = new Triangulo([6, 6, 6]);
-console.log(`Área del triángulo: ${triangulo.obtenerArea()}`);
-console.log(`Perímetro del triángulo: ${triangulo.obtenerPerimetro()}`);
-
-const cuadrado = new Cuadrado([5, 5, 5, 5]);
-console.log(`Área del cuadrado: ${cuadrado.obtenerArea()}`);
-console.log(`Perímetro del cuadrado: ${cuadrado.obtenerPerimetro()}`);
-
-const pentagono = new Pentagono([2, 2, 2, 2, 2]);
-console.log(`Área del pentágono: ${pentagono.obtenerArea()}`);
-console.log(`Perímetro del pentágono: ${pentagono.obtenerPerimetro()}`);
+  Reloj_instantaneo();
 
